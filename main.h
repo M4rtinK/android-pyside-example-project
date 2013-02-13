a440a7ada37bd495f236fd8f398c3c1d3816d330
Martin Kolman <martin.kolman@gmail.com>
2013-02-13 16:35:04 +0100

    import the project to Git

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define MAIN_PYTHON_FILE "/data/data/org.modrana.PySideExample/files/main.py"
+
+#define PYTHON_HOME "/data/data/org.modrana.PySideExample/files/python/"
+
+#define PYTHON_PATH "/data/data/org.modrana.PySideExample/files/python/lib/python2.7/lib-dynload:/data/data/org.modrana.PySideExample/files/python/lib/python2.7/:/data/data/org.modrana.PySideExample/files/python/lib/python2.7/site-packages:/data/data/org.modrana.PySideExample/files/python/lib"
+
+#define LD_LIBRARY_PATH "/data/data/org.modrana.PySideExample/files/python/lib:/data/data/org.modrana.PySideExample/files/python/lib/python2.7/lib-dynload:/data/data/org.kde.necessitas.ministro/files/qt/lib/"
+
+#define PATH "/data/data/org.modrana.PySideExample/files/python/bin:$PATH"
+
+#define THEME_PATH "/data/data/org.modrana.PySideExample/files/python/themes/"
+
+#define QML_IMPORT_PATH "/data/data/org.modrana.PySideExample/files/python/imports/"
+
+#endif // MAIN_H
